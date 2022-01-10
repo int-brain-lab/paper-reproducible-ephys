9bc31856bad023a63ee6f3dc2f9b922e67668da6
noamroth <39137729+noamroth@users.noreply.github.com>
2022-01-10 07:43:42 -0800

    consolidated plotting code

--- compute_num_trajectories.py
+++ compute_num_trajectories.py
@@ -78,6 +78,59 @@ trajectories = [trajectories[i] for i in np.where(num_regions >= min_regions)[0]
 
 print('With min regions >=3: ', len(trajectories), '(', len(trajectories) - previous_num, ')')
 
+#%%
+
+#currently hardcoded to match Steven & Guido analyses; 
+#todo: finalize numbers and match with above code
+num_trajectories = [92, -7, -16, -21, -7, -16, -32];
+
+
+#Plot fig 3a
+from matplotlib.sankey import Sankey
+plt.rcParams['svg.fonttype'] = 'none'
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1, xticks=[], yticks=[])
+sankey = Sankey(ax=ax, scale=0.01, offset=0.2, head_angle=90)
+sankey.add(flows=[92, -7, -16, -21, -7, -16, -32],
+           labels=['All sessions', 'Histology damage', 
+                   'Sessions per lab',
+                   'Noise/yield',
+                   'Targeting',
+                   'Behavior', 
+                   'Data analysis'],
+           orientations=[0, 1, -1, -1, -1,-1, 0],
+           pathlengths=[0.5, 0.2, 0.1, 0.2, 0.3, 0.5, 0.5],
+           facecolor = 'gray')  # Arguments to matplotlib.patches.PathPatch
+diagrams = sankey.finish()
+
+
+#text font and positioning
+for text in diagrams[0].texts:
+        text.set_fontsize('8')
+
+text = diagrams[0].texts[0]
+xy = text.get_position()
+text.set_position((xy[0] - 0.7, xy[1]))
+text.set_fontsize('10')
+
+text = diagrams[0].texts[-1]
+xy = text.get_position()
+text.set_position((xy[0] + 0.34, xy[1]))
+text.set_fontsize('10')
+
+text = diagrams[0].texts[2]
+xy = text.get_position()
+text.set_position((xy[0] + 0.2, xy[1]+0.05))
+
+text = diagrams[0].texts[3]
+xy = text.get_position()
+text.set_position((xy[0] + 0.06, xy[1]+0.02))
+
+
+
+plt.axis('off')
+plt.savefig(r'C:\Users\Steinmetz Lab User\Documents\GitHub\analysis\reproducible_ephys\sankey.svg', format="svg")
 
 
 #%% Do the corresponding BWM analysis
