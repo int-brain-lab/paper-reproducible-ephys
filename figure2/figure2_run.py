cebcdc62a8cc8fac800b17c523a88ed579865654
Mayo Faulkner <mayo.faulkner@ucl.ac.uk>
2022-05-02 15:16:15 +0100

    figure 1 now runs

--- figure2/figure2_run.py
+++ figure2/figure2_run.py
@@ -1,14 +1,11 @@
 """
 Script to prepare data for figure 2 and generate plots in the style that are shown in paper
 """
-import logging
 
 from figure2.figure2_prepare_data import prepare_data
 from figure2.figure2_plot_data import plot_hist_figure
 from reproducible_ephys_functions import get_histology_insertions, get_insertions
 from one.api import ONE
-
-logger = logging.getLogger('paper_reproducible_ephys')
 
 
 def run_figure2(one, recompute=False, supplementary=False):
@@ -17,7 +14,7 @@ def run_figure2(one, recompute=False, supplementary=False):
     prepare_data(insertions, one=one, recompute=recompute)
     plot_hist_figure(raw_histology=False)
     if supplementary:
-        logger.warning('Raw histology data to make figure 2 supplementary is not yet released')
+        print('Raw histology data to make figure 2 supplementary is not yet released')
 
 
 if __name__ == '__main__':
