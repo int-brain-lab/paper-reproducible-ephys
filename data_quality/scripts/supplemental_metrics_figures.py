c6ba2d7b327ea52a56369c6c176dc0dcb5b5a325
chris-langfield <christopher.langfield@internationalbrainlab.org>
2024-04-04 19:26:27 -0400

    final touches metrics supplemental figures

--- data_quality/scripts/supplemental_metrics_figures.py
+++ data_quality/scripts/supplemental_metrics_figures.py
@@ -4,6 +4,14 @@ from data_quality.tables import load_channels, load_clusters
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import string
+
+err_kws = {"markersize":20, 
+            "linewidth":1.0}
+
+def label_with_letter(ax, L):
+    ax.text(-0.3, -0.2, L, transform=ax.transAxes, 
+            size=16, weight='bold')
 
 def compute_yield_threeway(ibl_clusters, ibl_channels, 
                   ste_clusters, ste_channels, 
@@ -26,6 +34,10 @@ def compute_yield_threeway(ibl_clusters, ibl_channels,
     sites_ibl = ibl_channels.groupby("insertion").agg(num_sites=pd.NamedAgg(column="cosmos_acronym", aggfunc="count"))
     passing_per_site_ibl = passing_ibl.merge(sites_ibl, on="insertion")
 
+    all_ibl = ibl_clusters.groupby("insertion").count()["label"].to_numpy()
+    all_ste = ste_clusters.groupby("insertion").count()["label"].to_numpy()
+    all_aln = aln_clusters.groupby("insertion").count()["label"].to_numpy()
+
     passing_ste = ste_clusters.groupby("insertion").agg(passing_units = pd.NamedAgg(column="label", aggfunc=lambda x: len(x[x==1.])))
     sites_ste = ste_channels.groupby("insertion").agg(num_sites=pd.NamedAgg(column="cosmos_acronym", aggfunc="count"))
     passing_per_site_ste = passing_ste.merge(sites_ste, on="insertion")
@@ -38,6 +50,10 @@ def compute_yield_threeway(ibl_clusters, ibl_channels,
     passing_per_site_ste["passing_per_site"] = passing_per_site_ste["passing_units"]/passing_per_site_ste["num_sites"]
     passing_per_site_aln["passing_per_site"] = passing_per_site_aln["passing_units"]/passing_per_site_aln["num_sites"]
 
+    passing_per_site_ibl["all_units"] = all_ibl
+    passing_per_site_ste["all_units"] = all_ste
+    passing_per_site_aln["all_units"] = all_aln
+
 
     passing_per_site_ibl["source"] = "IBL"
     passing_per_site_ste["source"] = "Steinmetz"
@@ -45,7 +61,11 @@ def compute_yield_threeway(ibl_clusters, ibl_channels,
 
     return passing_per_site_ibl, passing_per_site_ste, passing_per_site_aln
 
-
+region_fullname = {
+    "Isocortex": "Cortex",
+    "TH": "Thalamus",
+    "HPF": "Hippocampus"
+}
 for region in ["Isocortex", "TH", "HPF"]:
 
     ibl_clusters = load_clusters("re_147", filter_region=region)
@@ -63,12 +83,16 @@ for region in ["Isocortex", "TH", "HPF"]:
 
     fig, axes = plt.subplots(2, 4)
 
+    passing_per_site_ibl, passing_per_site_ste, passing_per_site_aln = compute_yield_threeway(ibl_clusters, ibl_channels, 
+                                                                                            ste_clusters, ste_channels,
+                                                                                            aln_clusters, aln_channels)
+
     ## num insertions
     num_ins_ste = len(ste_clusters.index.levels[0])
     num_ins_aln = len(aln_clusters.index.levels[0])
     num_ins_ibl = len(ibl_clusters.index.levels[0])
     axes[0,0].bar(["IBL", "Steinmetz", "Allen"], [num_ins_ibl, num_ins_ste, num_ins_aln], color=colors)
-    axes[0,0].set_title("Number of insertions", fontsize=8)
+    axes[0,0].set_title("Number of insertions\nin region", fontsize=8)
     axes[0,0].set_xticklabels(["IBL", "STE", "ALN"], fontsize=8, rotation=90)
 
     ## sites per insertion (in this region)
@@ -76,10 +100,15 @@ for region in ["Isocortex", "TH", "HPF"]:
     sites_aln = aln_channels.groupby(["insertion"]).count()["cosmos_acronym"].to_numpy()
     sites_ibl = ibl_channels.groupby(["insertion"]).count()["cosmos_acronym"].to_numpy()
     sites = pd.DataFrame.from_dict(dict(zip(["IBL", "Steinmetz", "Allen"], [sites_ibl, sites_ste, sites_aln])), orient="index")
+    sns.pointplot(x="source", y="num_sites", data=passing_per_site_ste, ax=axes[0, 1], join=False, markersize=2, marker="none", capsize=.2, errorbar=("se", 1), color="black", err_kws=err_kws, zorder=100)
+    sns.pointplot(x="source", y="num_sites", data=passing_per_site_ibl, ax=axes[0, 1], join=False, markersize=2, marker="none", capsize=.2, errorbar=("se", 1), color="black", err_kws=err_kws, zorder=100)
+    sns.pointplot(x="source", y="num_sites", data=passing_per_site_aln, ax=axes[0, 1], join=False, markersize=2, marker="none", capsize=.2, errorbar=("se", 1), color="black", err_kws=err_kws, zorder=100)
     sns.stripplot(sites.T, ax=axes[0, 1], palette=colors_translucent)
-    axes[0, 1].set_title("Sites per insertion", fontsize=8)
+    axes[0, 1].set_title("Electrode sites in region\nper insertion", fontsize=8)
     axes[0, 1].set_xticklabels(["IBL", "STE", "ALN"], fontsize=8, rotation=90)
-    axes[0, 1].set_ylabel("channels in region")
+    axes[0, 1].set_xlabel(None)
+    axes[0, 1].set_ylabel(None)
+    #axes[0, 1].set_ylabel("Sites in region per insertion")
 
     ## units per insertion (in this region)
     units_per_ins_ibl = ibl_clusters.groupby(["insertion"]).count()["label"].to_numpy()
@@ -88,19 +117,23 @@ for region in ["Isocortex", "TH", "HPF"]:
 
     units_per_ins_all = pd.DataFrame.from_dict(dict(zip(["IBL", "Steinmetz", "Allen"], [units_per_ins_ibl, units_per_ins_ste, units_per_ins_aln])), orient="index")
 
+    sns.pointplot(x="source", y="all_units", data=passing_per_site_ste, ax=axes[0, 2], join=False, markersize=2, marker="none", capsize=.2, errorbar=("se", 1), color="black", err_kws=err_kws, zorder=100)
+    sns.pointplot(x="source", y="all_units", data=passing_per_site_ibl, ax=axes[0, 2], join=False, markersize=2, marker="none", capsize=.2, errorbar=("se", 1), color="black", err_kws=err_kws, zorder=100)
+    sns.pointplot(x="source", y="all_units", data=passing_per_site_aln, ax=axes[0, 2], join=False, markersize=2, marker="none", capsize=.2, errorbar=("se", 1), color="black", err_kws=err_kws, zorder=100)
+    
     sns.stripplot(units_per_ins_all.T, ax=axes[0, 2], palette=colors_translucent)
-    axes[0, 2].set_title("Units per insertion", fontsize=8)
+    axes[0, 2].set_title("Units in region per insertion", fontsize=8)
     axes[0, 2].set_xticklabels(["IBL", "STE", "ALN"], fontsize=8, rotation=90)
-    axes[0, 2].set_ylabel("units in region", fontsize=8)
+    #axes[0, 2].set_ylabel("Units in region per insertion", fontsize=8)
 
     ## units per site
     units_per_site_ibl = len(ibl_clusters) / len(ibl_channels)
     units_per_site_ste = len(ste_clusters) / len(ste_channels)
     units_per_site_aln = len(aln_clusters) / len(aln_channels)
     axes[0, 3].bar(["IBL", "Steinmetz", "Allen"], [units_per_site_ibl, units_per_site_ste, units_per_site_aln], color=colors)
-    axes[0, 3].set_title("Units per site", fontsize=8)
+    axes[0, 3].set_title("Units per site in region", fontsize=8)
     axes[0, 3].set_xticklabels(["IBL", "STE", "ALN"], fontsize=8, rotation=90)
-    axes[0, 3].set_ylabel("units/site in region", fontsize=8)
+    #axes[0, 3].set_ylabel("Units per site in region", fontsize=8)
 
     ## sliding RP metric
     ibl_SRP = ibl_clusters.slidingRP_viol
@@ -110,8 +143,8 @@ for region in ["Isocortex", "TH", "HPF"]:
     aln_SRP = aln_clusters.slidingRP_viol
     aln_frac = sum(aln_SRP) / len(aln_SRP)
     axes[1, 0].bar(["IBL", "Steinmetz", "Allen"], [ibl_frac, ste_frac, aln_frac], color=colors)
-    axes[1, 0].set_title("Sliding RP", fontsize=8)
-    axes[1, 0].set_ylabel("Fraction of units passing", fontsize=8)
+    axes[1, 0].set_title("Sliding RP QC", fontsize=8)
+    axes[1, 0].set_ylabel("Fraction of units in region passing", fontsize=8)
     axes[1, 0].set_xticklabels(["IBL", "STE", "ALN"], fontsize=8, rotation=90)
 
     ## noise cutoff
@@ -127,8 +160,8 @@ for region in ["Isocortex", "TH", "HPF"]:
     frac_aln = sum(aln_NC < 5.) / len(aln_NC)
     aln_NC = np.log10(aln_NC)
     axes[1, 1].bar(["IBL", "Steinmetz", "Allen"], [frac_ibl, frac_ste, frac_aln], color=colors)
-    axes[1, 1].set_title("Noise cutoff", fontsize=8)
-    axes[1, 1].set_ylabel("Fraction of units passing", fontsize=8)
+    axes[1, 1].set_title("Noise cutoff QC", fontsize=8)
+    axes[1, 1].set_ylabel("Fraction passing", fontsize=8)
     axes[1, 1].set_xticklabels(["IBL", "STE", "ALN"], fontsize=8, rotation=90)
 
     ## amplitude median
@@ -139,8 +172,8 @@ for region in ["Isocortex", "TH", "HPF"]:
     aln_amp = aln_clusters.amp_median.to_numpy() * 1e6
     frac_aln = sum(aln_amp > 50) / len(aln_amp)
     axes[1, 2].bar(["IBL", "Steinmetz", "Allen"], [frac_ibl, frac_ste, frac_aln], color=colors)
-    axes[1, 2].set_title("Amplitude median", fontsize=8)
-    axes[1, 2].set_ylabel("Fraction of units passing", fontsize=8)
+    axes[1, 2].set_title("Amplitude median QC", fontsize=8)
+    axes[1, 2].set_ylabel("Fraction passing", fontsize=8)
     axes[1, 2].set_xticklabels(["IBL", "STE", "ALN"], fontsize=8, rotation=90)
 
     ## passing units per site
@@ -153,23 +186,18 @@ for region in ["Isocortex", "TH", "HPF"]:
                                                                     passing_per_site_aln["passing_per_site"].to_numpy()]))
     passing_per_site_all = pd.DataFrame.from_dict(passing_per_site_all, orient="index")
 
-    mean_ibl = passing_per_site_ibl["passing_per_site"].mean()
-    mean_ste = passing_per_site_ste["passing_per_site"].mean()
-    mean_aln = passing_per_site_aln["passing_per_site"].mean()
-    stdev_ibl = passing_per_site_ibl["passing_per_site"].std() / np.sqrt(len(passing_per_site_ibl))
-    stdev_ste = passing_per_site_ste["passing_per_site"].std() / np.sqrt(len(passing_per_site_ste))
-    stdev_aln = passing_per_site_aln["passing_per_site"].std() / np.sqrt(len(passing_per_site_aln))
-
-    err_kws = {"markersize":20, 
-                "linewidth":1.5}
     sns.pointplot(x="source", y="passing_per_site", data=passing_per_site_ibl, ax=axes[1, 3], join=False, markersize=2, marker="none", capsize=.2, errorbar=("se", 1), color="black", err_kws=err_kws, zorder=100)
     sns.pointplot(x="source", y="passing_per_site", data=passing_per_site_ste, ax=axes[1, 3], join=False, markersize=2, marker="none", capsize=.2, errorbar=("se", 1), color="black", err_kws=err_kws, zorder=100)
     sns.pointplot(x="source", y="passing_per_site", data=passing_per_site_aln, ax=axes[1, 3], join=False, markersize=2, marker="none", capsize=.2, errorbar=("se", 1), color="black", err_kws=err_kws, zorder=100)
     sns.stripplot(passing_per_site_all.T, ax=axes[1, 3], palette=colors_translucent)
-    axes[1, 3].set_title("Passing units per site", fontsize=8)
+    axes[1, 3].set_title("QC passing neurons per site", fontsize=8)
     axes[1, 3].set_xticklabels(["IBL", "STE", "ALN"], fontsize=8, rotation=90)
     axes[1, 3].set_xlabel(None)
-    axes[1, 3].set_ylabel("insertion yield", fontsize=8)
+    axes[1, 3].set_ylabel(None)
 
-    fig.suptitle(f"IBL metrics: {region}")
+    if region=="TH":
+        for i, ax in enumerate(axes.flat):
+            label_with_letter(ax, string.ascii_uppercase[i])
+
+    fig.suptitle(f"IBL metrics: {region_fullname[region]}")
     fig.tight_layout()
